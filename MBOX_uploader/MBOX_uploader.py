9036c550e762000c2318030db9cfcbba3da83e76
Mike Brown <xenoxaos@gmail.com>
2013-05-13 15:20:01 -0300

    Update to 0.6
    Features added:
    Follows subfolders and creates subfolder/nested labels
    Checks to see if any mbox files have already been processed and skips them
    Checks if an individual email message has already been uploaded (can speed up resuming of large mbox files)

--- MBOX_uploader/MBOX_uploader.py
+++ MBOX_uploader/MBOX_uploader.py
@@ -1,6 +1,9 @@
+import re
 import codecs
 import email
 import email.header
+import email.mime.application as ponies
+from email.mime.multipart import MIMEMultipart
 import getpass
 import imaplib
 import locale
@@ -14,19 +17,26 @@ import time
 import unicodedata
 import urllib
 import os
+import md5
+import pickle
 from optparse import OptionParser
 from urlparse import urlparse
 
-__version__ = "0.1"
+__version__ = "0.6"
+#TODO: Possibly parse the .msf (Mork) files associated with Thunderbird to get folder names with invalid characters in them.
+#TODO: Set size limit for email attachments (20MB) and remove or attempt to compress them and reattach as .zip/.xz
+
 
 if sys.version_info < (2, 5):
-    print >>sys.stderr, "UDel MBOX Uploader requires Python 2.5 or later."
+    print >>sys.stderr, "MBOX Uploader requires Python 2.5 or later."
     sys.exit(1)
 
 class MyOptionParser(OptionParser):
     def __init__(self):
         usage = "usage: python %prog [options]\n"\
-                "  Utility to upload multiple Thunderbird/*nix style MBOX files\n"\
+                "  MBOX UNIX style mbox file.\n"\
+                "  DEST is imap[s]://[USER[:PASSWORD]@]HOST[:PORT][/BOX]\n"\
+                "  DEST has a priority over the options."
         OptionParser.__init__(self, usage,
                               version="UDel MBOX Uploader " + __version__)
         self.add_option("--user", help="login name [default: empty]")
@@ -35,8 +45,8 @@ class MyOptionParser(OptionParser):
         self.add_option("--path", help="Path to MBOX files [default: ./mbox")
         self.set_defaults(user = "",
                          password = "",
-                         logfile = "MBOX_log.txt",
-                         path = "./mbox")
+                         logfile = "MBOX_log",
+                         path = ".\mbox")
 
 
 
@@ -110,8 +120,9 @@ class Progress():
         decoded = []
         try:
             parts = email.header.decode_header(sbj)
+
             for s, codec in parts:
-                decoded.append(s.decode(codec or "ascii"))
+                decoded.append(s.decode(codec or "ascii").encode('ascii'))
         except Exception, e:
             pass
         return "".join(decoded)
@@ -140,18 +151,30 @@ class Progress():
 
 
 
-def upload(imap, src, err, time_fields, f):
+def upload(imap, src, err, time_fields, f, message_digests, mbox_hash):
     print >>sys.stderr, \
           "Counting the mailbox (it could take a while for the large one)."
+
     p = Progress(len(src))
     for i, msg in src.iteritems():
         try:
+            msg_hash = md5.md5(msg.as_string()).hexdigest()
+            print md5.md5(msg.as_string()).hexdigest()
+
+
             p.begin(msg, f)
+            #check if the hash combo already exists, if it is, don't reupload
+            if ( mbox_hash + msg_hash in message_digests):
+                print >>f,"message already uploaded"
+                p.endNg("Already Uploaded",f)
+                continue
             r, r2 = imap.upload(msg.get_delivery_time(time_fields),
                                 msg.as_string(), 3)
+
             if r != "OK":
-                raise Exception(r2[0])
+                raise Exception(r2[0]) # FIXME: Should use custom class
             p.endOk(f)
+            message_digests.add( mbox_hash + msg_hash)
             continue
         except socket.error, e:
             p.endNg("Socket error: " + str(e), f)
@@ -226,6 +249,8 @@ class IMAPUploader:
         self.user = user
         self.password = password
         self.retry = 3
+        self.connectTime = time.time()
+        self.mailbox_list = set()
 
 
 
@@ -234,13 +259,9 @@ class IMAPUploader:
             retry = self.retry
         try:
             self.open()
-            (answer,status) = self.imap.select(self.box)
-            if answer == 'NO':
-              try:
-                self.imap.create(self.box)
-              except:
-                sys.exit(1)
-	    return self.imap.append(self.box, [], delivery_time, message)
+            self.check_reconnect()
+            self.imap.select(self.box)
+            return self.imap.append(self.box, [], delivery_time, message)
         except (imaplib.IMAP4.abort, socket.error):
             self.close()
             if retry == 0:
@@ -249,16 +270,83 @@ class IMAPUploader:
         time.sleep(5)
         return self.upload(delivery_time, message, retry - 1)
 
-    def change_mailbox(self, mboxfile):
-        self.box = mboxfile
+    def create_subfolder(self, folder_name):
+        try:
+            self.open()
+            self.check_reconnect()
+            (answer,status) = self.imap.select(folder_name)
+            if answer == 'NO':
+              try:
+                self.imap.create(folder_name)
+                self.get_mailbox()
+                return True
+              except:
+                sys.exit(1)
+            return False
+        except (imaplib.IMAP4.abort, socket.error):
+            self.close()
+
+    def get_mailbox(self):
+        #get a list of mailboxes that are currently on the IMAP server
+        imap_list = self.imap.list()
+        for item in imap_list[1]:
+            item = re.split('" "',item)
+            item = item[len(item)-1]
+            item = item[:-1]
+            self.mailbox_list.add(item)
+
+    def mailbox_exist(self, mailbox):
+        #if the mailbox exists on the server, return the name given by the
+        #server to prevent any issues with mailbox Case
+        for box in self.mailbox_list:
+            if box.upper() == mailbox.upper():
+                return (box, True)
+        return (mailbox, False)
+
+    def change_mailbox(self, mboxfile, log):
+        special_mailboxes = {"Starred", "Important", "All Mail", "Drafts", "Trash", "Sent Mail"}
+        mailbox_string = ""
+        #split apart the mailbox and make sure that all parent mailboxes exist
+        #or create the parent mailbox
+        mailbox_parts =  mboxfile.split('/')
+        for parts in mailbox_parts:
+            #check for special mailboxes that exist in Gmail
+            for special in special_mailboxes:
+                if parts == special:
+                    parts = "Uploaded_" + parts
+                    break
+            #Can't have a mailbox start with a space
+            mailbox_string += parts.strip()
+            #Cant have multiple spaces in a mailbox line
+            mailbox_string = re.sub(' +', ' ', mailbox_string)
+
+            #check if the wanted inbox exists already and if it does, store the
+            #case that the server has, so Inbox => INBOX, etc
+            #if it doesn't exist, create it
+            (mailbox_string, exists) = self.mailbox_exist(mailbox_string)
+            if exists == False:
+                print "***Creating Subfolder: " + mailbox_string
+                print >>log, "***Creating Subfolder: " + mailbox_string
+                self.create_subfolder(mailbox_string)
+            mailbox_string += "/"
+
+        #This should always be the case, but just checking
+        if mailbox_string.endswith("/"):
+            mailbox_string = mailbox_string[:-1]
+
+        self.box = mailbox_string
+        print "***Changing to mailbox: %s"% mailbox_string
+        print >>log, "Changing to mailbox: %s"% mailbox_string
 
     def open(self):
         if self.imap:
             return
+        self.connectTime = time.time()
         imap_class = [imaplib.IMAP4, imaplib.IMAP4_SSL][self.ssl];
         self.imap = imap_class(self.host, self.port)
         self.imap.socket().settimeout(60)
         self.imap.login(self.user, self.password)
+        self.get_mailbox()
 
     def close(self):
         if not self.imap:
@@ -266,6 +354,33 @@ class IMAPUploader:
         self.imap.shutdown()
         self.imap = None
 
+    def check_reconnect(self):
+        #Having issues with HUGE mbox files (>2G) timing out.
+        #If the connection has been up for longer than 15 min, reconnect
+        if (time.time() - self.connectTime > 15*60):
+            self.close()
+            self.open()
+
+def format_mailbox(subdirname, file, sourcedir):
+
+    if subdirname.startswith(sourcedir):
+        subdirname = subdirname[len(sourcedir)+1:]
+    #Mozilla puts .sbd on directories, so we'll strip it out
+    subdirname = subdirname.replace('.sbd', '')
+    #convert backslashes to forward slashes for imap
+    subdirname = subdirname.replace('\\', '/')
+    subdirname += "/"
+    subdirname += file
+    #fix for having an '&' in the mailbox name
+    subdirname = subdirname.replace('&', '&-')
+    #The carat '^' is not allowed either
+    subdirname = subdirname.replace('^', ' ')
+    #Probably put an over abundance of these things at the beginning and end...
+    if subdirname.startswith('/'):
+        subdirname = subdirname[1:]
+    if subdirname.endswith('/'):
+        subdirname = subdirname[:-1]
+    return subdirname
 
 def main(args=None):
     try:
@@ -288,53 +403,110 @@ def main(args=None):
             options.user = sys.stdin.readline().rstrip("\n")
         if len(str(options.password)) == 0:
             options.password = getpass.getpass()
-        #options = options.__dict__
+
         sourcedir = options.path
         logname = options.logfile +".txt"
-        logfile = open(logname, 'w')
-        err = logfile + "_failed_messages.mbox"
+
+        logfile = open(logname, 'a')
+        #Try to read filenames from completed.log for files we can skip
+        completed_items = [""]
+        try:
+            completed_mbox = open("completed.log", 'r')
+
+            for line in completed_mbox:
+                completed_items.append(line.strip())
+            completed_mbox.close()
+        except:
+            print "No previous logs"
+        completed_mbox = open("completed.log", 'a')
+
+        errname = options.logfile + "_failed_messages.mbox"
+
         usern = options.user
         passwordn = options.password
+
+
         time_fields=["from", "received", "date"]
-        # Connect to the server and login
-        print >>sys.stderr, \
-              "Connecting to IMAP Server as %s" % usern
+        start_time = time.time()
+        #send current time in epoch format
+        print >>logfile, start_time
+
+        print >>sys.stderr,"Connecting to IMAP Server as %s" % usern
         print >>logfile,'Connecting to IMAP Server as %s'% usern
 
+        boxnames = dict()
+
+        #get the set that's been pickled already for uploaded individual messages.
+        message_digests = set()
+        try:
+            with open('message_hash.bin', 'rb') as input:
+                message_digests = pickle.load(input)
+        except:
+            print "no previous message data"
+
+        # Connect to the server and login
         uploader = IMAPUploader(usern, passwordn)
         uploader.open()
-        totalMessages = 0
-        totalMBOXFiles = 0
-        print >>logfile, "List of MBOX files and message Counts:"
-        for r,d,files in os.walk(sourcedir):
-            for file in files:
-                totalMBOXFiles +=1
-                thisMBOXMessages = 0
-                sourcefile = os.path.join(r,file)
-                mboxfile = mailbox.mbox(sourcefile, create=False)
-                for message in mboxfile:
-                    totalMessages +=1
-                    thisMBOXMessages +=1
-                print >>logfile, 'MBOX: %s %d'% (left_fit_width(file + ":",80, ' '), thisMBOXMessages)
-        print >>logfile, "\n"
-        print >>logfile, 'Total MBOX Files: %d'% totalMBOXFiles
-        print >>logfile, 'Total Messages: %d'% totalMessages
-        print >>logfile, "\n"
+        for stuff in uploader.mailbox_list:
+            print stuff
 
-
+        try:
+            os.mkdir("Failed Messages")
+        except:
+            print "Failed Messages directory already exists"
         for r,d,files in os.walk(sourcedir):
-            for file in files:
-                uploader.change_mailbox(file)
-                print "mailboxname:" + file
+             for file in files:
+                #open the current file as a mbox file
                 sourcefile = os.path.join(r,file)
                 src = mailbox.mbox(sourcefile, create=False)
-                if err:
-                    err = mailbox.mbox(err)
+
+                #check if the mbox file has no messages and skip it if true
+                if len(src) is 0:
+                    continue
+
+                #Get the foldername that corresponds to the current mbox file
+                #Change to that mailbox on the IMAP server and create it if
+                #it doesnt exist
+                folderName = format_mailbox(r, file, sourcedir)
+                uploader.change_mailbox(folderName, logfile)
+
+                #add message count to the dict
+                boxnames[sourcefile] = len(src)
+
+                #Create a mbox file for rejected mail messages for review later
+                err = mailbox.mbox(os.path.join("Failed Messages", folderName.replace("/", "_") + "_" + errname))
+
                 # Upload
-                print >>logfile, "Uploading messages in file %s\n"% file
-                print >>sys.stderr, "Uploading..." + file
-                upload(uploader, src, err, time_fields, logfile)
+                print >>logfile, "Uploading messages in file %s\n"% sourcefile
+                print >>sys.stderr, "Uploading " + file
+
+                #hash the name of the mbox file+path
+                mbox_hash = md5.md5(sourcefile).hexdigest()
+
+                upload(uploader, src, err, time_fields, logfile, message_digests, mbox_hash)
+
+                #If nothing went into the err mbox file, delete it.
+                if len(err) is 0:
+                    err.close()
+                    os.remove(os.path.join("Failed Messages", folderName.replace("/", "_") + "_" + errname))
                 print >>logfile, "\n"
+
+                #write the completed filename to completed.log for resume purposes
+                print >>completed_mbox, sourcefile
+
+
+        #write a summary of mailboxes and message counts
+        for box in boxnames:
+            print >>logfile, "%s: %s messages"% (box, boxnames[box])
+        print "\n\n\n\nMail transfer is now complete. You may close this window now.\n\n\n"
+        print >>logfile, time.time()
+
+        m, s = divmod(time.time() - start_time, 60)
+        h, m = divmod(m, 60)
+
+        print >>logfile, "%d:%02d:%02d" % (h, m, s)
+        with open('message_hash.bin', 'wb') as output:
+            pickle.dump(message_digests, output, pickle.HIGHEST_PROTOCOL)
         return 0
     except optparse.OptParseError, e:
         print >>sys.stderr, e
